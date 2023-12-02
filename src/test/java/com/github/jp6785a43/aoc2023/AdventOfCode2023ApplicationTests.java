ec2f72fe6782650db759ff22d82b84791b3ef14a
jp6785a43 <no@thank.you>
2023-12-02 11:44:01 -0500

    init

---
+++ src/test/java/com/github/jp6785a43/aoc2023/AdventOfCode2023ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.jp6785a43.aoc2023;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdventOfCode2023ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
